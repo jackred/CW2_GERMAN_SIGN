a6acb01322ebf0e579ea576a28d61aaf3a213995
Jackred <jackred@tuta.io>
2019-11-28 18:02:43 +0000

    UPDATE keras ann for cross validation

--- src/keras_ANN.py
+++ src/keras_ANN.py
@@ -18,27 +18,36 @@ from keras.utils import plot_model
 from math import sqrt
 
 
-def keras_ann(x_train, y_train, x_test, epochs=1, dry=False):
-    dim = int(sqrt(len(x_train[0])))
+def keras_ann_predict(model, x_test):
+    dim = int(sqrt(len(x_test[0])))
     if K.image_data_format() == 'channels_first':
-        x_train = x_train.reshape(x_train.shape[0], 1, dim, dim)
         x_test = x_test.reshape(x_test.shape[0], 1, dim, dim)
         input_shape = (1, dim, dim)
     else:
-        x_train = x_train.reshape(x_train.shape[0], dim, dim, 1)
         x_test = x_test.reshape(x_test.shape[0], dim, dim, 1)
         input_shape = (dim, dim, 1)
-    x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
-    x_train /= 255
     x_test /= 255
+    print(x_test.shape[0], 'test samples')
+    return model.predict(x_test)
+
+
+def keras_ann_model(x_train, y_train, epochs=1):
+    dim = int(sqrt(len(x_train[0])))
+    if K.image_data_format() == 'channels_first':
+        x_train = x_train.reshape(x_train.shape[0], 1, dim, dim)
+        input_shape = (1, dim, dim)
+    else:
+        x_train = x_train.reshape(x_train.shape[0], dim, dim, 1)
+        input_shape = (dim, dim, 1)
+    x_train = x_train.astype('float32')
+    x_train /= 255
     # param
     num_classes = len(np.unique(y_train))
     batch_size = 1+(len(x_train) // 100)
     y_train = keras.utils.to_categorical(y_train, num_classes)
     print('x_train shape:', x_train.shape)
     print(x_train.shape[0], 'train samples')
-    print(x_test.shape[0], 'test samples')
     #
     # build model
     #
@@ -62,8 +71,7 @@ def keras_ann(x_train, y_train, x_test, epochs=1, dry=False):
               epochs=epochs,
               verbose=1,
               validation_split=0.3)
-    model.summary()
-    return model.predict(x_test), model
+    return model
 
 
 def main():
@@ -71,8 +79,9 @@ def main():
     rand = np.random.randint(10000000)
     data_train, data_test = helper.pre_processed_data_all(args, rand)
     label_train, label_test = helper.pre_processed_label_all(args, rand)
-    predicted, model = keras_ann(data_train, label_train, data_test,
-                                 epochs=args.epochs)
+    model = keras_ann_model(data_train, label_train, epochs=args.epochs)
+    model.summary()
+    predicted = keras_ann_predict(model, data_test)
     predicted = helper.score_to_class(predicted)
     helper.compare_class(predicted, label_test)
     plot_model(model)
