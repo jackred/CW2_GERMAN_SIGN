7a717300a45c5592d371904503f8095cc3c83e41
No2 <noe.tiger@free.fr>
2019-12-06 13:54:16 +0000

    updating to be able to make the tests

--- src/linear_classifier.py
+++ src/linear_classifier.py
@@ -1,28 +1,47 @@
 from sklearn.linear_model import SGDClassifier
 
-from helper import pre_processed_data_all, pre_processed_label_all, \
-    compare_class_true_positive, compare_class, measure, true_positive, false_positive
+from helper import pre_processed_data_all, pre_processed_label_all, print_result, tree_to_png, run_function, extract_measures, plot_experiment_server
 from arg import rForest_args
 
 import numpy as np
 import random
 
-def linear_classifier(data_train, label_train, data_test):
-    clf = SGDClassifier(loss='modified_huber')
+def linear_classifier(data_train, label_train, data_test, max_iter):
+    clf = SGDClassifier(loss='modified_huber', n_jobs=6, n_iter_no_change=max_iter)
     clf.fit(data_train, label_train)
-    return clf.predict(data_test), clf.predict_proba(data_test)
+    return clf.predict(data_test)#, clf.predict_proba(data_test)
+
+
+def server():
+	args = rForest_args()
+	rand = np.random.randint(10000000)
+	data_train, data_test = pre_processed_data_all(args, rand)
+	label_train, label_test = pre_processed_label_all(args, rand)
+	res = []
+	for i in range(5):
+		print('===\n=====Epochs: %d=====\n===' % i)
+		res.append(run_function(linear_classifier,
+					args.cross_validate,
+					data_train, label_train,
+					data_test, label_test, max_iter=(i + 1)))
+	print(res)
+	res = extract_measures(res)
+	print(res)
+	plot_experiment_server('linear_classifier_test', 'max iteration ( x 100)', res)
 
 if __name__ == "__main__":
-    print('start linear classifier')
-    args = rForest_args()
-    rand = np.random.randint(100000)
-    data_train, data_test = pre_processed_data_all(args, rand)
-    label_train, label_test = pre_processed_label_all(args, rand)
-    print('data loaded')
-    found, confidence = linear_classifier(data_train, label_train, data_test)
-    print('linear classifier done')
-    compare_class_true_positive(found, label_test)
-    compare_class(found, label_test)
-    measure(found, label_test, confidence, True)
-    print(true_positive(found, label_test), true_positive(found, label_test, True))
-    print(false_positive(found, label_test), false_positive(found, label_test, True))
+    server()
+
+    # print('start linear classifier')
+    # args = rForest_args()
+    # rand = np.random.randint(100000)
+    # data_train, data_test = pre_processed_data_all(args, rand)
+    # label_train, label_test = pre_processed_label_all(args, rand)
+    # print('data loaded')
+    # found, confidence = linear_classifier(data_train, label_train, data_test)
+    # print('linear classifier done')
+    # compare_class_true_positive(found, label_test)
+    # compare_class(found, label_test)
+    # measure(found, label_test, confidence, True)
+    # print(true_positive(found, label_test), true_positive(found, label_test, True))
+    # print(false_positive(found, label_test), false_positive(found, label_test, True))
--- src/random_forest.py
+++ src/random_forest.py
@@ -1,32 +1,48 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
 
-from helper import pre_processed_data_all, pre_processed_label_all, \
-    compare_class_true_positive, compare_class, measure
+from helper import pre_processed_data_all, pre_processed_label_all, print_result, tree_to_png, run_function, extract_measures, plot_experiment_server
 from arg import rForest_args
 
 import numpy as np
 import random
 
-def random_forest(data_train, label_train, data_test):
+def random_forest(data_train, label_train, data_test, n_estimator):
     clf = RandomForestClassifier(
-        n_estimators=100,
-        # max_depth=10,
+        n_estimators=n_estimator,
         max_features=10
-        # min_samples_leaf=10
     )
     clf.fit(data_train, label_train)
-    return clf.predict(data_test), clf.predict_proba(data_test)
+    return clf.predict(data_test)# , clf.predict_proba(data_test)
+
+def server():
+	args = rForest_args()
+	rand = np.random.randint(10000000)
+	data_train, data_test = pre_processed_data_all(args, rand)
+	label_train, label_test = pre_processed_label_all(args, rand)
+	res = []
+	for i in range(30):
+		print('===\n=====Epochs: %d=====\n===' % i)
+		res.append(run_function(random_forest,
+					args.cross_validate,
+					data_train, label_train,
+					data_test, label_test, n_estimator=i+1))
+	print(res)
+	res = extract_measures(res)
+	print(res)
+	plot_experiment_server('random_forest_test_9000', 'n estimator', res)
 
 if __name__ == "__main__":
-    print('start random tree classification')
-    args = rForest_args()
-    rand = np.random.randint(100000)
-    data_train, data_test = pre_processed_data_all(args, rand)
-    label_train, label_test = pre_processed_label_all(args, rand)
-    print('data loaded')
-    found, confidence = random_forest(data_train, label_train, data_test)
-    print('random tree done')
-    compare_class_true_positive(found, label_test)
-    compare_class(found, label_test)
-    measure(found, label_test, confidence, True)
+    server()
+
+    # print('start random tree classification')
+    # args = rForest_args()
+    # rand = np.random.randint(100000)
+    # data_train, data_test = pre_processed_data_all(args, rand)
+    # label_train, label_test = pre_processed_label_all(args, rand)
+    # print('data loaded')
+    # found, confidence = random_forest(data_train, label_train, data_test)
+    # print('random tree done')
+    # compare_class_true_positive(found, label_test)
+    # compare_class(found, label_test)
+    # measure(found, label_test, confidence, True)
